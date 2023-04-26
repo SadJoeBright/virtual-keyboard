4cb93b4eb76cf1e55e9b2a60ed6b2769f8b759b8
SadJoeBright <sadjoebright.gmail.com>
2023-04-26 03:25:01 +0300

    feat: functionality of the shift-key is implemented

--- script.mjs
+++ script.mjs
@@ -1,46 +1,89 @@
 import keysData from './keysData.mjs';
 
-function initDomStructure() {
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('keyboard-wrapper');
-  document.body.prepend(wrapper);
+let lang = localStorage.getItem('lang') || 'eng';
+let keyCase = 'caseDown';
+const wrapper = document.createElement('div');
+wrapper.classList.add('keyboard-wrapper');
+document.body.prepend(wrapper);
 
-  const title = document.createElement('h1');
-  title.classList.add('title');
-  title.innerText = 'RSS Virtual Keyboard';
-  wrapper.append(title);
+const title = document.createElement('h1');
+title.classList.add('title');
+title.innerText = 'RSS Virtual Keyboard';
+wrapper.append(title);
 
-  const textArea = document.createElement('textarea');
-  textArea.classList.add('textarea');
-  textArea.setAttribute('autofocus', 'true');
-  textArea.setAttribute('cols', '50');
-  textArea.setAttribute('rows', '5');
-  wrapper.append(textArea);
+const textArea = document.createElement('textarea');
+textArea.classList.add('textarea');
+textArea.setAttribute('autofocus', 'true');
+textArea.setAttribute('cols', '50');
+textArea.setAttribute('rows', '5');
+wrapper.append(textArea);
 
-  const keyboard = document.createElement('div');
-  keyboard.classList.add('keyboard');
-  wrapper.append(keyboard);
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+wrapper.append(keyboard);
 
-  for (let i = 0; i < keysData.length; i += 1) {
-    const keyboardRow = document.createElement('div');
-    keyboardRow.classList.add('keyboard__row');
-    for (let j = 0; j < keysData[i].length; j += 1) {
-      const keyboardKey = document.createElement('div');
-      keyboardKey.classList.add('keyboard__key', keysData[i][j].className);
-      keyboardKey.innerText = keysData[i][j].eng.caseDown;
-      keyboardRow.append(keyboardKey);
-    }
-    keyboard.append(keyboardRow);
+for (let i = 0; i < keysData.length; i += 1) {
+  const keyboardRow = document.createElement('div');
+  keyboardRow.classList.add('keyboard__row');
+  for (let j = 0; j < keysData[i].length; j += 1) {
+    const keyboardKey = document.createElement('div');
+    keyboardKey.classList.add('keyboard__key', keysData[i][j].className);
+    keyboardKey.innerText = keysData[i][j][`${lang}`].caseDown;
+    keyboardRow.append(keyboardKey);
   }
-
-  const description = document.createElement('p');
-  description.classList.add('description');
-  description.innerHTML = 'The keyboard was created in <strong>Windows</strong>';
-  wrapper.append(description);
-  const langInstruction = document.createElement('p');
-  langInstruction.classList.add('description');
-  langInstruction.innerHTML = 'To switch the languge press <strong>LeftAlt + LeftShift</strong>';
-  wrapper.append(langInstruction);
+  keyboard.append(keyboardRow);
 }
 
-window.addEventListener('load', initDomStructure);
+const description = document.createElement('p');
+description.classList.add('description');
+description.innerHTML = 'The keyboard was created in <strong>Windows</strong>';
+wrapper.append(description);
+const langInstruction = document.createElement('p');
+langInstruction.classList.add('description');
+langInstruction.innerHTML = 'To switch the languge press <strong>LeftAlt + LeftShift</strong>';
+wrapper.append(langInstruction);
+
+function activate(event) {
+  // event.preventDefault();
+  // if (event.code === )
+  document.querySelector(`.${event.code}`).classList.add('keyboard__key_active');
+  // textArea.value += event.key;
+}
+function deactivate(event) {
+  // event.preventDefault();
+  document.querySelector(`.${event.code}`).classList.remove('keyboard__key_active');
+}
+
+function printWithMouse(event) {
+  textArea.value += event.target.innerHTML;
+}
+
+function toggleLang() {
+  lang = lang === 'eng' ? 'rus' : 'eng';
+  for (let i = 0; i < keysData.length; i += 1) {
+    for (let j = 0; j < keysData[i].length; j += 1) {
+      document.querySelectorAll('.keyboard__row')[i].querySelectorAll('.keyboard__key')[j].innerText = keysData[i][j][`${lang}`][keyCase];
+    }
+  }
+  localStorage.setItem('lang', lang);
+}
+const rightShift = document.querySelector('.ShiftRight');
+const leftShift = document.querySelector('.ShiftLeft');
+function toggleShift(event) {
+  event.target.classList.toggle('keyboard__key_active');
+  keyCase = keyCase === 'caseDown' ? 'caseUp' : 'caseDown';
+  for (let i = 0; i < keysData.length; i += 1) {
+    for (let j = 0; j < keysData[i].length; j += 1) {
+      document.querySelectorAll('.keyboard__row')[i].querySelectorAll('.keyboard__key')[j].innerText = keysData[i][j][`${lang}`][keyCase];
+    }
+  }
+}
+
+rightShift.addEventListener('click', toggleShift);
+leftShift.addEventListener('click', toggleShift);
+document.querySelector('.Space').addEventListener('click', toggleLang);
+
+document.querySelectorAll('.keyboard__key').forEach((key) => key.addEventListener('click', printWithMouse));
+document.addEventListener('keydown', activate);
+document.addEventListener('keyup', deactivate);
+document.addEventListener('click', () => textArea.focus());
